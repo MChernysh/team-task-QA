93b69f05d51091951e76eea5d583040d9cd545d3
OrestDiachok <chaingang@mail.ru>
2013-03-05 14:11:50 +0200

    Task12

--- Task/src/test/java/Team/Task/ResultPage.java
+++ Task/src/test/java/Team/Task/ResultPage.java
@@ -1,6 +1,7 @@
 package Team.Task;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.List;
 import java.util.regex.Matcher;
