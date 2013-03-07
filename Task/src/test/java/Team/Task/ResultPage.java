32437473d75639417cc69197c7a61486fd6184cf
tarasoleksyn <taras.el@gmail.com>
2013-03-07 16:08:22 +0000

    Task7 - DDT update

--- Task/src/test/java/Team/Task/ResultPage.java
+++ Task/src/test/java/Team/Task/ResultPage.java
@@ -1,8 +1,13 @@
 package Team.Task;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -10,7 +15,6 @@ import java.util.regex.Pattern;
 public class ResultPage {
 	
 	private WebDriver driver;
-	
 	ResultPage(WebDriver driver){
 		
 		this.driver = driver;
@@ -53,5 +57,17 @@ public class ResultPage {
 	public void clearTextBox(By by){
 		
 		driver.findElement(by).clear();
+	}
+	
+	public void makeScreenshot(String filename){
+		
+		File file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);	
+		try {
+			FileUtils.copyFile(file, new File(filename));
+		} 
+		catch (IOException e) 
+		{ 
+			e.printStackTrace(); 
+		} 
 	}
 }
--- Task/src/test/java/Team/Task/Task7.java
+++ Task/src/test/java/Team/Task/Task7.java
@@ -1,31 +1,79 @@
 package Team.Task;
+
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.AssertJUnit;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 
 public class Task7{
 
 	private FirefoxDriver driver = new FirefoxDriver();
-	private String searchTerm = "samsung";
 	private String login = "testatqc@gmail.com";
 	private String password = "IF-025.ATQC";
 	
 	HomePage home = new HomePage(driver);
 	
+	public static List<String> getExcelData(String filename, int colNum){
+		
+		int sheetNum = 0; // worksheet number
+		List<String> values = new ArrayList<String>();
+		
+		try {
+			
+			FileInputStream file = new FileInputStream(new File(filename));
+			
+			HSSFWorkbook workbook = new HSSFWorkbook(file);
+			HSSFSheet sheet = workbook.getSheetAt(sheetNum);
+			
+			for(Row r : sheet) {
+			   
+				Cell c = r.getCell(colNum);
+			   
+			   if(c != null) {
+			      if(c.getCellType() == Cell.CELL_TYPE_STRING) {
+			         values.add(c.getStringCellValue());
+			      }
+			      else {
+			    	  System.out.println(c.getStringCellValue() + " isn't a string!");
+			    	  System.exit(1);
+			      }
+			   }
+			}
+		} 
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} 
+		catch (IOException e) {
+		  	e.printStackTrace();
+		}
+		
+		return values;
+	}
+	
+	
 	public boolean isElementIn(By by) {
 		
 		driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
 		boolean present = driver.findElements(by).size() != 0;
-		driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		
 		return present;
 	}
@@ -51,11 +99,31 @@ public class Task7{
 	@AfterClass
 	public void tearDown(){
 		
+		home.deleteWishLists(By.xpath("//div[@class=\"cell wishlist-i-delete\"]/a[@name=\"wishlist-delete\"]"));
 		driver.close();
 	}
 	
-	@Test
-	public void Task7_Test(){
+	@DataProvider(name = "data")
+	public static Object[][] init() {
+		
+		List<String> list = getExcelData("src/test/data/test.xls",0);
+		
+		// remove first cell entry - "Test7", "Test1" ... etc
+		list.remove(0);	
+		
+		Object[][] result=new Object[list.size()][];
+		int i = 0;
+		
+		for(String s:list){
+		    result[i] = new Object[]{new String(s)};
+		    i++;
+		   }	
+		
+		return result;	
+	}
+	
+	@Test (dataProvider = "data")
+	public void Test(String searchTerm){
 		
 		// clear the text box and search for the searchTerm
 		home.clearTextBox(By.xpath(".//div//input[@class=\"text\"]"));
@@ -72,27 +140,23 @@ public class Task7{
 		// get 3..5 search results and store them to the wish list
 		for(int i = 3; i <= 5; i++) {														
 																							 
-			result.clickElement(By.xpath(".//table["+i+"]//a[@name=\"towishlist\"]")); 				
-			result.clearTextBox(By.xpath(".//input[@name=\"wishlist_title\"]"));
-			result.sendText(By.xpath(".//input[@name=\"wishlist_title\"]"), "Список желаний "+ (i-2));
-			result.clickElement(By.xpath(".//div[@class=\"submit\"]/button[@type=\"submit\"]"));
-	
-		  if(i < 5) 
-			result.clickElement(By.xpath(".//a[@name=\"close\"]"));
+			if(isElementIn(By.xpath(".//table[" + i + "]//a[@name=\"towishlist\"]"))){
+				
+				result.clickElement(By.xpath(".//table[" + i + "]//a[@name=\"towishlist\"]")); 				
+				result.clearTextBox(By.xpath(".//input[@name=\"wishlist_title\"]"));
+				result.sendText(By.xpath(".//input[@name=\"wishlist_title\"]"), "Список желаний " + (i-2));
+				result.clickElement(By.xpath(".//div[@class=\"submit\"]/button[@type=\"submit\"]"));	
+			
+			if(i < 5) 
+				result.clickElement(By.xpath(".//a[@name=\"close\"]"));
+			}
 		}
 		
 		// visit the wish list page from the pop up
 		result.clickElement(By.xpath(".//div[@class=\"comment\"]/a[@class=\"underline\"]"));			
 
-		//make a screenshot and save it to the project's directory
-		File file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);	
-		try {
-			FileUtils.copyFile(file, new File("test-output/Task_7 - Screenshot.png"));
-		} 
-		catch (IOException e) 
-		{ 
-			e.printStackTrace(); 
-		} 
+		// make a screenshot and save it to the project's directory
+		result.makeScreenshot("test-output/Task_7 - Screenshot.png");
 	}	
 }
 
