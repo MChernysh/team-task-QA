2cf17070abff7fe1b4d873ab022c5eaed3737261
DmitryCher <d_chernyshov@mail.ru>
2013-03-09 04:15:23 +0200

    DDT Update for TeamTask

--- Task/src/test/java/Team/Task/Task11.java
+++ Task/src/test/java/Team/Task/Task11.java
@@ -11,31 +11,72 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.Reporter;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook; 
 
 public class Task11 {
 
 	private WebDriver driver= new FirefoxDriver();
 	HomePage homepage = new HomePage(driver);
 
-	@BeforeTest
+	@BeforeClass 
 	public void settingUp() {
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("http://rozetka.com.ua");
-		homepage.SignIN("testatqc@gmail.com", "IF-025.ATQC");
 	}
 
 	
-	@AfterTest
+	@AfterClass
 	public void turnDown() {
 		driver.quit();
 	}
 
+	
+	@DataProvider(name = "ExcelData")
+    public Object[][] createData() throws Exception{
+        Object[][] retObjArr=getTableArray(".\\src\\test\\data\\test.xls", "Data", "Test11");
+        return(retObjArr);
+    }
+	
+	
+	@Test(dataProvider = "ExcelData") 
+	public void LogIn(String Login, String Password) {
+		driver.findElement(By.name("signin")).click();
+	  	driver.findElement(By.name("login")).clear();
+		driver.findElement(By.name("login")).sendKeys(Login);
+		driver.findElement(By.name("password")).clear(); 
+		driver.findElement(By.name("password")).sendKeys(Password);		
+		driver.findElement(By.xpath("//*[text()='Войти']")).click();
 		
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		if (driver.findElements(By.name("signout")).size() != 0) {
+			Reporter.log("Account with login  " + Login + "  and password  " + Password +"  exists!");			
+			driver.findElement(By.name("signout")).click();
+			}
+			else if (driver.findElements(By.xpath("//div[@class='message code4']")).size() != 0) {
+					Reporter.log("Account with login  " +  Login + "  is  not registered!");					
+					driver.findElement(By.xpath("//*[text()='Отмена']")).click();
+					}
+					else if (driver.findElements(By.xpath("//div[@class='message code5']")).size() != 0) {
+							Reporter.log("Password  " + Password + "  for login  " + Login + "  is incorrect!");							
+							driver.findElement(By.xpath("//*[text()='Отмена']")).click();
+					}
+		
+	}
+	
+	
 	@Test(groups = { "Dima" })
 	public void task11() {
+		
+		homepage.SignIN("testatqc@gmail.com", "IF-025.ATQC");
 		//Select "Computers" from main menu
 		driver.findElement(By.xpath(".//*[@id='computers-notebooks']")).click();
 
@@ -61,7 +102,7 @@ public class Task11 {
 		//Select the third notebook from top list
 		String topthree = driver.findElement(By.xpath("//div[@class='item available popular3']//div[@class='title']")).getText();
 		System.out.println("The third element in Top is: " + topthree);
-
+		Reporter.log("The third element in Top is: " + topthree);
 		
 		//Move mouse to the third element in top list
 		Actions builder = new Actions(driver);
@@ -72,12 +113,12 @@ public class Task11 {
 		
 		//Check the popup and show its contents 
 		driver.findElement(By.xpath("html/body/div[6]/div/div/div[1]/p")).getText();
-		//System.out.println("Popup window contains such information: " + "\n" + "'" + popup + "'");		
+//		System.out.println("Popup window contains such information: " + "\n" + "'" + popup + "'");		
 		
 		//Check the hyperlink in popup and show its content
 		driver.findElement(By.xpath("html/body/div[6]/div/div/div[1]/div/a")).getText();
-		System.out.println("Hyperlink exists!");
-		
+//		System.out.println("Hyperlink exists!");
+		Reporter.log("Hyperlink exists!");
 		
 		//Make screenshot
 		File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
@@ -98,10 +139,9 @@ public class Task11 {
 
 		//Check the USD price
 		String usd = driver.findElement(By.xpath(".//*[@id='head_banner_container']//div/span/div")).getText();
-		System.out.println("The price is: " + usd);
-
+//		System.out.println("The price is: " + usd);
+		Reporter.log("The price is: " + usd);
 	}
-	
 	
 	
 	
@@ -110,11 +150,48 @@ public class Task11 {
 			Assert.assertEquals(frststr, scndstr);
 		} 
 		catch (AssertionError e) {
-			System.out.println("Strings \n" + "'" + frststr + "'" + "\n and \n" + "'" + scndstr + "'" + "\n are not equal!");
-			//Reporter.log("Strings <br>" + "'" + frststr + "'" + "<br> and <br>" + "'" + scndstr + "'" + "<br> are not equal!<br>");
+//			System.out.println("Strings \n" + frststr + "\n and \n" + scndstr + "\n are not equal!");
+			Reporter.log("Strings \n" + frststr + "\n and \n" + scndstr + "\n are not equal!");
 		}
 	}
 	
 	
-	
+	public String[][] getTableArray(String xlFilePath, String sheetName, String tableName){
+        String[][] tabArray=null;
+        try{
+            Workbook workbook = Workbook.getWorkbook(new File(xlFilePath));
+            Sheet sheet = workbook.getSheet(sheetName);
+            int startRow,startCol, endRow, endCol,ci,cj;
+            Cell tableStart=sheet.findCell(tableName);
+            startRow=tableStart.getRow();
+            startCol=tableStart.getColumn();
+
+            Cell tableEnd= sheet.findCell(tableName, startCol+1,startRow+1, 100, 64000,  false);                               
+
+            endRow=tableEnd.getRow();
+            endCol=tableEnd.getColumn();
+//          System.out.println("startRow=" + startRow + ", endRow=" + endRow + ", " + 
+//                  "startCol=" + startCol + ", endCol=" + endCol);
+            Reporter.log("startRow=" + startRow + ", endRow=" + endRow + ", " + 
+                    "startCol=" + startCol + ", endCol=" + endCol);
+            tabArray=new String[endRow-startRow-1][endCol-startCol-1];
+            ci=0;
+
+            for (int i=startRow+1;i<endRow;i++,ci++){
+                cj=0;
+                for (int j=startCol+1;j<endCol;j++,cj++){
+                    tabArray[ci][cj]=sheet.getCell(j,i).getContents();
+                }
+            }
+        }
+        catch (Exception e) {
+//          System.out.println("Error in getTableArray()!");
+            Reporter.log("Error in getTableArray()!");
+        }
+
+        return(tabArray);
+    }
+
+
+		
 }
