daa15a12b3e9a3988e47862be49601de98fe1be9
tarasoleksyn <taras.el@gmail.com>
2013-03-13 22:57:06 +0000

    task7 update

--- Task/src/test/java/Team/Task/ResultPage.java
+++ Task/src/test/java/Team/Task/ResultPage.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.TakesScreenshot;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,17 +17,18 @@ public class ResultPage {
 	
 	private WebDriver driver;
 	
-	ResultPage(WebDriver driver){
+	ResultPage(WebDriver driver) {
 		
 		this.driver = driver;
 	}
 	
-	String getElementText(By by){
+	String getElementText(By by) {
 		
-		return driver.findElement(by).getText();
+		String text = driver.findElement(by).getText(); 
+		return text;
 	}
 	
-	public void clickElement(By by){
+	public void clickElement(By by) {
 		
 		driver.findElement(by).click();
 	}
@@ -36,31 +38,32 @@ public class ResultPage {
 		driver.findElement(by).sendKeys(text);
 	}
 
-	public int stringToInt(String str){
+	public int stringToInt(String str) {
 	
-	Pattern p = Pattern.compile("(\\d+)");
+		Pattern p = Pattern.compile("(\\d+)");
         Matcher m = p.matcher(str);
-         int res = 0;
-         if (m.find()) res = Integer.valueOf(m.group(1));
+        int res = 0;
+        if (m.find()) { 
+        	res = Integer.valueOf(m.group(1));
+        }
         return res;
 	}
 
 	public boolean isElementPresent(By by) {
-	List<WebElement> act = driver.findElements(by);
+		List<WebElement> act = driver.findElements(by);
 	
-	if (!act.isEmpty()) 
-		return true; 
-	else 
-		return false;
-
+		if (!act.isEmpty()) 
+			return true; 
+		else 
+			return false;
 	}
 	
-	public void clearTextBox(By by){
+	public void clearTextBox(By by) {
 		
 		driver.findElement(by).clear();
 	}
 	
-	public void makeScreenshot(String filename){
+	public void makeScreenshot(String filename) {
 		
 		File file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);	
 		try {
@@ -70,5 +73,14 @@ public class ResultPage {
 		{ 
 			e.printStackTrace(); 
 		} 
+	}
+	
+	public int getElementCount(By by) {
+		
+		driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+		int count = driver.findElements(by).size();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		return count;
 	}
 }
--- Task/src/test/java/Team/Task/Task7.java
+++ Task/src/test/java/Team/Task/Task7.java
@@ -14,13 +14,13 @@ import jxl.Workbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class Task7{
+public class Task7 {
 
 	private FirefoxDriver driver = new FirefoxDriver();
 	private String login = "testatqc@gmail.com";
 	private String password = "IF-025.ATQC";
 	
-	HomePage home = new HomePage(driver);
+	HomePage homePage = new HomePage(driver);
 	
 	public boolean isElementIn(By by) {
 		
@@ -32,33 +32,35 @@ public class Task7{
 	}
 	
 	@BeforeClass
-	public void setUp(){
+	public void setUp() {
 		
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.get("http://rozetka.com.ua");
+		System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.Jdk14Logger");
 		
 		// sign in and close the annoying social media pop up
-		home.SignIN(login, password);
-		home.clickElement(By.xpath(".//a[@name=\"close\"]"));		
+		homePage.SignIN(login, password);
+		homePage.clickElement(By.xpath(".//a[@name='close']"));		
 		Reporter.log("Log in: " + login + " Password: " + password);
 
-		// go to wish list page, check whether there are some and delete them
-		home.clickElement(By.xpath(".//a[@name=\"profile\"]"));
-		home.clickElement(By.xpath("//div[@class=\"title\"]/a[contains(text(), \"Списки желаний\")]"));
+		// go to the wish list page, check whether there are some and delete them
+		homePage.clickElement(By.xpath(".//a[@name='profile']"));
+		homePage.clickElement(By.xpath("//div[@class='title']/a[contains(text(), 'Списки желаний')]"));
 		
-		if(isElementIn(By.xpath("//div[@class=\"cell wishlist-i-delete\"]/a[@name=\"wishlist-delete\"]")))
-			home.deleteWishLists(By.xpath("//div[@class=\"cell wishlist-i-delete\"]/a[@name=\"wishlist-delete\"]"));	
+		if (isElementIn(By.xpath("//div[@class='cell wishlist-i-delete']/a[@name='wishlist-delete']"))) {
+			homePage.deleteWishLists(By.xpath("//div[@class='cell wishlist-i-delete']/a[@name='wishlist-delete']"));
+		}
 	}
 	
 	@AfterClass
-	public void tearDown(){
+	public void tearDown() {
 		
-		home.deleteWishLists(By.xpath("//div[@class=\"cell wishlist-i-delete\"]/a[@name=\"wishlist-delete\"]"));
-		Reporter.log("Deleting wishlists");
+		homePage.deleteWishLists(By.xpath("//div[@class='cell wishlist-i-delete']/a[@name='wishlist-delete']"));
+		Reporter.log("Deleting wish lists");
 		driver.close();
 	}
 	
-	public String[][] getTableArray(String xlFilePath, String sheetName, String tableName) throws Exception{
+	public String[][] getTableArray(String xlFilePath, String sheetName, String tableName) throws Exception {
         	
 		String[][] tabArray = null;
         
@@ -81,65 +83,78 @@ public class Task7{
         
         ci = 0;
 
-            for (int i = startRow + 1; i < endRow; i++, ci++){
+            for (int i = startRow + 1; i < endRow; i++, ci++) {
                 cj = 0;
-                for (int j = startCol + 1; j < endCol; j++, cj++){
+                for (int j = startCol + 1; j < endCol; j++, cj++) {
                     tabArray[ci][cj] = sheet.getCell(j,i).getContents();
                 }
             }
         
         return(tabArray);
     }
-
+	
 	@DataProvider(name = "data")
-    public Object[][] createData1() throws Exception{
+    public Object[][] createData() throws Exception {
         
-		Object[][] retObjArr = getTableArray(".\\src\\test\\data\\test.xls", "Data", "test7Data");
+		Object[][] retObjArr = getTableArray("./src/test/data/test.xls", "Data", "test7Data");
         
 		return(retObjArr);
     }
 	
-	@Test (dataProvider = "data")
-	public void Test(String searchTerm){
+	@Test(dataProvider = "data") 
+	public void Test(String searchTerm) {
 		
 		// clear the text box and search for the searchTerm
-		home.clearTextBox(By.xpath(".//div//input[@class=\"text\"]"));
-		home.sendText(By.xpath(".//div//input[@class=\"text\"]"), searchTerm);
+		homePage.clearTextBox(By.xpath(".//div//input[@class='text']"));
+		homePage.sendText(By.xpath(".//div//input[@class='text']"), searchTerm);
 		Reporter.log("Searching for: " + searchTerm);
 		
 		// click "Submit" button and verify whether search results contain the searchTerm
-		ResultPage result = home.clickElement(By.xpath(".//button[@type=\"submit\"]")); 				
-		AssertJUnit.assertTrue(searchTerm.equals(result.getElementText(By.xpath(".//h1/span")))); 	 
+		ResultPage resultPage = homePage.clickElement(By.xpath(".//button[@type='submit']")); 				
+		AssertJUnit.assertTrue(searchTerm.equals(resultPage.getElementText(By.xpath(".//h1/span")))); 	 
 		Reporter.log("Validating: " + searchTerm);
 
 		// get the element's color and verify it
 		String color = driver.findElement(By.xpath(".//h1/span")).getCssValue("color");	
 		AssertJUnit.assertTrue(color.equals("rgba(50, 154, 28, 1)"));						
 		Reporter.log("Validating color: " + color);
-			
-		// get 3..5 search results and store them to the wish list
-		for(int i = 3; i <= 5; i++) {														
+
+		String wishlistName;
+		int elemNum = 3; // number of products to be added to the wish list 
+		int offset = 2;
+		
+		// get 3 search results beginning with the third position and store them to the wish list
+		for (int i = 1; i <= elemNum; i++) {														
 																							 
-			if(isElementIn(By.xpath(".//table[" + i + "]//a[@name=\"towishlist\"]"))){
+			if (isElementIn(By.xpath(".//table[" + (i + offset) + "]//a[@name='towishlist']"))) {
+
+				wishlistName = "Список желаний " + i;
+				resultPage.clickElement(By.xpath(".//table[" + (i + offset) + "]//a[@name='towishlist']")); 				
+				int wishlistCount = resultPage.getElementCount(By.xpath(".//label/input"));
 				
-				String wishlist = "Список желаний " + (i-2);
+				// check whether an appropriate number of wish lists exists, if not, create them
+				if (wishlistCount == elemNum) {
+					resultPage.clickElement(By.xpath(".//ul/li[" + i + "]/label/input")); // select the existing wish list
+				} else {
+					resultPage.clearTextBox(By.xpath(".//input[@name='wishlist_title']"));
+					resultPage.sendText(By.xpath(".//input[@name='wishlist_title']"), wishlistName);	// create new wish list
+				}
 				
-				result.clickElement(By.xpath(".//table[" + i + "]//a[@name=\"towishlist\"]")); 				
-				result.clearTextBox(By.xpath(".//input[@name=\"wishlist_title\"]"));
-				result.sendText(By.xpath(".//input[@name=\"wishlist_title\"]"), wishlist);
-				result.clickElement(By.xpath(".//div[@class=\"submit\"]/button[@type=\"submit\"]"));
-				Reporter.log("Creating wishlist: " + wishlist);
-			
-			if(i < 5) 
-				result.clickElement(By.xpath(".//a[@name=\"close\"]"));
+				// add product to the selected wish list
+				resultPage.clickElement(By.xpath(".//div[@class='submit']/button[@type='submit']"));	
+				Reporter.log("Creating wish list: " + wishlistName);
+							
+				if (i < elemNum) {
+					resultPage.clickElement(By.xpath(".//a[@name='close']"));
+				}
 			}
 		}
 		
-		// visit the wish list page from the pop up
-		result.clickElement(By.xpath(".//div[@class=\"comment\"]/a[@class=\"underline\"]"));			
+		// go to the wish list page from the pop up
+		resultPage.clickElement(By.xpath(".//div[@class='comment']/a[@class='underline']"));			
 
 		// make a screenshot and save it to the project's directory
-		result.makeScreenshot("target/screenshots/task_7.png");
+		resultPage.makeScreenshot("target/screenshots/task_7.png");
 		Reporter.log("Taking a screenshot");
 	}	
 }
